35d3f0cb4149fa8dee13a9a6d54fd0577bb7a53e
Talha Brother <talhaikram262005@gmail.com>
2025-11-09 15:39:41 +0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 setTimeout(()=>{
-    alert("DISCLAIMER: THIS PAGE WAS MADE AS A PERSONAL EDUCATIONAL PROJECT. This is NOT the official site of the company or brand identified on the page. The creator of this page is NOT affiliated with the company or brand in any way. This page is a personal project made in connection with an educational exercise.");
-
+ Swal.fire({
+    icon: 'info',
+    title: 'DISCLAIMER',
+    text: 'THIS PAGE WAS MADE AS A PERSONAL EDUCATIONAL PROJECT. This is NOT the official site of the company or brand identified on the page. The creator of this page is NOT affiliated with the company or brand in any way. This page is a personal project made in connection with an educational exercise.',
+    confirmButtonText: 'OK',
+    confirmButtonColor: '#3085d6'
+  });
 },1000)
 
 
@@ -9,5 +14,3 @@ gsap.set(".a", { opacity: 0, x: -100 });
 gsap.to(".image img", {  x: -40,opacity:1, duration: 1 });
 gsap.to(".a", {  x:40,opacity:1, duration: 1 });
 
-// gsap.to(".right", {  x: -60, duration: 1 });
-// gsap.to(".left", {  x: 60, duration: 1 });
